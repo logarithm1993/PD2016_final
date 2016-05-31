e537e70e9c1e4e1dd490f24cccaedb1b436fe172
OAO <andyandyhk@hotmail.com>
2016-05-31 17:02:40 +0800

    fix tabStop->space

--- src/Element.cpp
+++ src/Element.cpp
@@ -48,23 +48,23 @@
 int
 Node::centerX() const
 {
-	//TODO
-	if(_curOrt == N || _curOrt == S || _curOrt == FN || _curOrt == FS)
-	  return _curCord._x + _width/2;
-	else
-	  return _curCord._x + _height/2;
-	return 0;
+    //TODO
+    if(_curOrt == N || _curOrt == S || _curOrt == FN || _curOrt == FS)
+        return _curCord._x + _width/2;
+    else
+    return _curCord._x + _height/2;
+    return 0;
 }
 
 int
 Node::centerY() const
 {
-	//TODO
-	if(_curOrt == N || _curOrt == S || _curOrt == FN || _curOrt == FS)
-	  return _curCord._y + _height/2;
-	else
-	  return _curCord._y + _width/2;
-	return 0;
+    //TODO
+    if(_curOrt == N || _curOrt == S || _curOrt == FN || _curOrt == FS)
+        return _curCord._y + _height/2;
+    else
+        return _curCord._y + _width/2;
+    return 0;
 }
 
 /**Pin Function*********************************************************
@@ -82,11 +82,11 @@ Node::centerY() const
 double
 Pin::x() const
 {
-	//TODO
-	double x = (double)_node->_curCord._x;
+    //TODO
+    double x = (double)_node->_curCord._x;
     double w = (double)_node->_width;
-	double h = (double)_node->_height;
-	switch(_node->_curOrt) {
+    double h = (double)_node->_height;
+    switch(_node->_curOrt) {
         case N :  return x + _offsetX; 
         case W :  return x + h - _offsetY;
         case S :  return x + w - _offsetX;
@@ -95,26 +95,26 @@ Pin::x() const
         case FW:  return x + h - _offsetY;
         case FS:  return x + _offsetX;
         case FE:  return x + _offsetY;
-	}
+    }
 }
 
 double
 Pin::y() const
 {
-	//TODO	
+    //TODO
     double y = (double)_node->_curCord._y;  
     double w = (double)_node->_width;
-	double h = (double)_node->_height;
-  switch(_node->_curOrt) {
+    double h = (double)_node->_height;
+    switch(_node->_curOrt) {
         case N :  return y + _offsetX; 
-		case W :  return y + _offsetX;
-		case S :  return y + h - _offsetY;
-		case E :  return y + w - _offsetX;
-		case FN:  return y +  _offsetY;
-		case FW:  return y + w - _offsetX;
-		case FS:  return y + h - _offsetY;
-		case FE:  return y + _offsetX;
-	}
+        case W :  return y + _offsetX;
+        case S :  return y + h - _offsetY;
+        case E :  return y + w - _offsetX;
+        case FN:  return y +  _offsetY;
+        case FW:  return y + w - _offsetX;
+        case FS:  return y + h - _offsetY;
+        case FE:  return y + _offsetX;
+    }
 }
 
 /**Net Function*********************************************************
@@ -132,19 +132,19 @@ Pin::y() const
 double
 Net::computeHPWL() const
 {
-	//TODO
-	double xmax = DBL_MIN;
+    //TODO
+    double xmax = DBL_MIN;
     double xmin = DBL_MAX;
     double ymax = DBL_MIN;
     double ymin = DBL_MAX;
-	for(size_t i = 0, psize = _pinList.size(); i < psize; ++i) {
+    for(size_t i = 0, psize = _pinList.size(); i < psize; ++i) {
         Pin* p = _pinList[i];
-		if(p->x() > xmax) xmax = p->x();
-		if(p->x() < xmin) xmin = p->x();
-		if(p->y() > ymax) ymax = p->y();
-		if(p->y() < ymin) ymin = p->y();
-	}
-	return xmax - xmin + ymax - ymin;
+        if(p->x() > xmax) xmax = p->x();
+        if(p->x() < xmin) xmin = p->x();
+        if(p->y() > ymax) ymax = p->y();
+        if(p->y() < ymin) ymin = p->y();
+    }
+    return xmax - xmin + ymax - ymin;
 }
 
 /**Term Function********************************************************
