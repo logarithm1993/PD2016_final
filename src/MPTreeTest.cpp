aab775a21d0d552bd3353b572946c0a66c09eedd
nianzelee <nianzelee@gmail.com>
2016-06-10 22:14:43 +0800

    NZ : refactor swap subtrees

--- src/MPTreeTest.cpp
+++ src/MPTreeTest.cpp
@@ -49,27 +49,28 @@ using namespace std;
 void
 MPTreeMgr::test()
 {
-   cout << " >  testing MPTree : \n";
-   Node * pNd1 , * pNd2;
-   int arg1 , arg2;
+   //cout << " >  testing MPTree : \n";
+   //Node * pNd1 , * pNd2;
+   //int arg1 , arg2;
 
-   pNd1 = pNd2 = NULL;
-   arg1 = arg2 = -1;
+   //pNd1 = pNd2 = NULL;
+   //arg1 = arg2 = -1;
    /*cout << "rotate a node : \n";
    perturbMPTree( &pNd1 , &pNd2 , &arg1 , &arg2 , 0 );
    cout << "node name = " << pNd1->_name 
         << " old Ort = "   << arg1 
         << " new Ort =  "  << Nz_Orient2Int( pNd1->_curOrt ) << endl;
    cout << "undo: \n" << endl;
-   perturbMPTree( &pNd1 , &pNd2 , &arg1 , &arg2 , 0 );
+   undoMPTree( &pNd1 , &pNd2 , &arg1 , &arg2 , 0 );
    cout << "node name = " << pNd1->_name << " ort =  " << Nz_Orient2Int( pNd1->_curOrt ) << endl;*/
-   //printMPTree();
-   /*swapSubTree_int( 2 , 3 );
-   swapSubTree_int( 3 , 2 );
-   swapSubTree_int( 0 , 3 );
-   swapSubTree_int( 3 , 0 );
-   swapSubTree_int( 1 , 2 );
-   swapSubTree_int( 2 , 1 );
+   
+   /*printMPTree();
+   cout << "swap two subtrees : \n";
+   perturbMPTree( &pNd1 , &pNd2 , &arg1 , &arg2 , 3 );
+   cout << "swap " << arg1 << " and " << arg2 << endl;
+   printMPTree();
+   undoMPTree( &pNd1 , &pNd2 , &arg1 , &arg2 , 3 );
+   cout << "undo : \n";
    printMPTree();*/
 	//writeAllNode( cout );
 	//writeAllTerm( cout );
